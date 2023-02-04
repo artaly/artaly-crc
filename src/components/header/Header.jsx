aa2eb109b3f9fff1f7fd35ddc4b7235dd1e0203a
artaly <villalonalycah@gmail.com>
2023-02-04 19:35:16 +0800

    Add mobile responsiveness for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+
+    const [Toggle, showMenu] = useState(false);
+
     return (
         <header className="header">
             <nav className="nav container">
                 <a href="index.html" className="nav__logo">Artaly</a>
-                <div className="nav__menu">
+                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list grid">
                         <li className="nav__item">
-                            <a href="#about" className="nav__link">
+                            <a href="#about" className="nav__link active-link">
+                                
                                 <span className="li__number">01. </span>About
                             </a>
                         </li>
@@ -35,6 +39,14 @@ const Header = () => {
                             </a>
                         </li>
                     </ul>
+
+                    <i 
+                    className="uil uil-times nav__close"
+                    onClick={() => showMenu(!Toggle)}></i>
+                </div>
+
+                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+                    <i className="uil uil-apps"></i>
                 </div>
             </nav>
         </header>

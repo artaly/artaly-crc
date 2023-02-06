ee671c60c310c93bfdeefc5c59f364e5a6a3ee6a
artaly <villalonalycah@gmail.com>
2023-02-06 15:11:49 +0800

    Finalize structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import About from './components/about/About';
 import Header from './components/header/Header';
+import Education from './components/education/Education';
+import Experience from './components/experience/Experience';
 import Home from './components/home/Home';
+import Skills from './components/skills/Skills';
+import Cerification from './components/certification/Cerification';
 
 function App() {
   return (
@@ -8,6 +13,11 @@ function App() {
       <Header/>
       <main className='main'>
         <Home/>
+        <About/>
+        <Education/>
+        <Experience/>
+        <Skills/>
+        <Cerification/>
       </main>
     </>
   );

b43d12d10a6f8b0434c88a0fcbba2f8f8c0de7e5
artaly <villalonalycah@gmail.com>
2023-02-06 15:09:41 +0800

    Add skills

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './skills.css'
+
+const Skills = () => {
+  return (
+    <section class="section skills" id='skills'>
+  <div class="section__title">04. Skills</div>
+  <div class="section__content">
+    <div class="skillz">
+      <div class="skillz__category">
+        <div class="skillz__category__label">Languages</div>
+        <ul>
+          <li class="skillz__category__item">Dart</li>
+          <li class="skillz__category__item">Java</li>
+          <li class="skillz__category__item">Javascript</li>
+          <li class="skillz__category__item">HTML</li>
+          <li class="skillz__category__item">CSS</li>
+          <li class="skillz__category__item">Python</li>
+
+        </ul>
+      </div>
+      <div class="skillz__category">
+        <div class="skillz__category__label">Frameworks</div>
+        <ul>
+            <li class="skillz__category__item">Flutter</li>
+            <li class="skillz__category__item">React</li>
+            <li class="skillz__category__item">Tailwind</li>
+        </ul>
+      </div>
+      <div class="skillz__category">
+        <div class="skillz__category__label">Tools</div>
+        <ul>
+          <li class="skillz__category__item">Git and Github</li>
+          <li class="skillz__category__item">BASH</li>
+          <li class="skillz__category__item">AWS</li>
+          <li class="skillz__category__item">Azure</li>
+          <li class="skillz__category__item">SQL</li>
+          <li class="skillz__category__item">mySQL</li>
+          <li class="skillz__category__item">Firebase</li>
+        </ul>
+      </div>
+      <div class="skillz__category">
+        <div class="skillz__category__label">Design</div>
+        <ul>
+          <li class="skillz__category__item">DEDEDE</li>
+          <li class="skillz__category__item">DEDEDE</li>
+          <li class="skillz__category__item">DEDEDE</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
+  )
+}
+
+export default Skills

f0c8f1b7f3fb3385d1352ea4c9ec6075eea94552
artaly <villalonalycah@gmail.com>
2023-03-02 16:30:08 +0800

    Update lambda for PUT

--- aws-sam/put-function/main.go
+++ aws-sam/put-function/main.go
@@ -1,55 +1,54 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io"
-	"net/http"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-)
 
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(sess)
+
+	input := &dynamodb.UpdateItemInput{
+		TableName: aws.String("cloud-resume-db"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"ID": {
+				S: aws.String("visitors"),
+			},
+		},
+		UpdateExpression: aws.String("ADD visitors :inc"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":inc": {
+				N: aws.String("1"),
+			},
+		},
+	}
+
+	_, err := svc.UpdateItem(input)
+
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
+		log.Fatalf("Got error calling UpdateItem: %s", err)
 	}
 
 	return events.APIGatewayProxyResponse{
-		Headers: map[string]string {
-			"Access-Control-Allow-Origin": "*",
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":  "*",
 			"Access-Control-Allow-Methods": "*",
 			"Access-Control-Allow-Headers": "*",
 		},
-		Body:       fmt.Sprintf("{ \"count\": \"2\"}"),
 		StatusCode: 200,
 	}, nil
 }
+
 
 func main() {
 	lambda.Start(handler)

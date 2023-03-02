bd2059da67b0ae3d6f108f26aeeb96e49f89d761
artaly <villalonalycah@gmail.com>
2023-03-02 16:55:18 +0800

    Update fetch

--- aws-sam/resume-site/src/scripts/fetch.js
+++ aws-sam/resume-site/src/scripts/fetch.js
@@ -1,5 +1,6 @@
-fetch('https://102ewhf875.execute-api.us-east-1.amazonaws.com/Prod/get')
-.then(response => response.json())
-.then((data) => {
-    document.getElementById('visitor_count').innerText = data.count
-})
+fetch('https://102ewhf875.execute-api.us-east-1.amazonaws.com/Prod/put')
+            .then(() => fetch(' https://102ewhf875.execute-api.us-east-1.amazonaws.com/Prod/get'))
+            .then(response => response.json())
+            .then((data) => {
+                document.getElementById('visitor_count').innerText = data.count
+            })
